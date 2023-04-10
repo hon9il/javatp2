58c76bd359fbfe42a8b8b2a161e20f5b1dd70be6
hngl <hon9il.kim@gmail.com>
2023-04-10 18:27:35 -0400

    commit #6 Hongil
    j'ai ajoute les fonctions F1 de Laura

--- IFT1025-TP2/src/main/java/client/Client.java
+++ IFT1025-TP2/src/main/java/client/Client.java
@@ -18,7 +18,71 @@ public class Client {
         objectInputStream = new ObjectInputStream(socket.getInputStream());
     }
 
-    // Code de F1 ici !!
+    // Méthode pour récupérer la liste des cours disponibles pour une session donnée
+    public void showAvailableCourses() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            boolean continueLoop = true;
+            while (continueLoop) {
+                System.out.println("Veuillez choisir la session pour laquelle vous voulez consulter la liste des cours");
+                System.out.println("1. Automne");
+                System.out.println("2. Hiver");
+                System.out.println("3. Ete");
+
+                int session = scanner.nextInt();
+                String sessionChoisie = "";
+
+                if(session == 1){
+                    sessionChoisie = "Automne";
+                }
+                if(session == 2){
+                    sessionChoisie = "Hiver";
+                }
+                if(session == 3){
+                    sessionChoisie = "Ete";
+                }
+
+                // Envoyer la commande handleLoadCourses au serveur avec le semestre demandé
+                objectOutputStream.writeObject(Server.LOAD_COMMAND + " " + sessionChoisie);
+                objectOutputStream.flush();
+
+                // Afficher la liste de cours
+                System.out.println("Les cours offerts pendant la session d' " + sessionChoisie + " sont:");
+
+                // Lire la liste de cours renvoyée par le serveur
+                ArrayList<Course> courses;
+                courses = (ArrayList<Course>) objectInputStream.readObject();
+                if (courses.isEmpty()) {
+                    System.out.println("Aucun cours disponible pour la session sélectionnée.");
+                } else {
+                    for (Course courseDisponible : courses) {
+                        System.out.println(courseDisponible);
+                    }
+                }
+
+                // Demander à l'utilisateur s'il souhaite s'inscrire à un cours ou charger des cours pour un autre semestre
+
+                System.out.println("Pour continuer:");
+                System.out.println("1. Consulter les cours offerts pour une autre session");
+                System.out.println("2. Inscription à un cours");
+
+                int choix = scanner.nextInt();
+
+                if (choix == 1) {
+                    continueLoop = true;
+                } else if (choix == 2) {
+                    registerStudentToCourse();
+                    continueLoop = false;
+                } else {
+                    System.out.println("Choix invalide. Veuillez entrer 1, 2 ou 3.");
+                    continueLoop = true;
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     // Méthode pour enregistrer un étudiant à un cours
     public void registerStudentToCourse() {
@@ -60,7 +124,8 @@ public class Client {
         try {
             Client client = new Client("localhost", 9090);
 
-            // Tu peux appeler ton méthode F1 ici !!
+            // Appeler la méthode d'inscription
+            client.showAvailableCourses();
 
             // Appeler la méthode d'inscription
             client.registerStudentToCourse();

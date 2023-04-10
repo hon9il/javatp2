a97e3647875fa39e92110ee3646fb49c9b871d8a
laura <lauracadillo13@gmail.com>
2023-04-10 15:11:33 -0400

    commit #7 Laura
    ignore the last commit, this is the one

--- IFT1025-TP2/src/main/java/server/Client.java
+++ IFT1025-TP2/src/main/java/server/Client.java
@@ -22,64 +22,70 @@ public class Client {
     }
 
     // Méthode pour récupérer la liste des cours disponibles pour une session donnée
-    public void showAvailableCourses(){
-        try{
-
+    public void showAvailableCourses() {
+        try {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Veuillez choisir la session pour laquelle vous voulez consulter la liste des cours");
-            System.out.println("1. Automne");
-            System.out.println("2. Hiver");
-            System.out.println("3. Ete");
+            boolean continueLoop = true;
+            while (continueLoop) {
+                System.out.println("Veuillez choisir la session pour laquelle vous voulez consulter la liste des cours");
+                System.out.println("1. Automne");
+                System.out.println("2. Hiver");
+                System.out.println("3. Ete");
 
-            int session = scanner.nextInt();
-            String sessionChoisie = "";
+                int session = scanner.nextInt();
+                String sessionChoisie = "";
 
-            if(session == 1){
-                sessionChoisie = "Automne";
-            }
-            if(session == 2){
-                sessionChoisie = "Hiver";
-            }
-            if(session == 3){
-                sessionChoisie = "Ete";
-            }
+                if(session == 1){
+                    sessionChoisie = "Automne";
+                }
+                if(session == 2){
+                    sessionChoisie = "Hiver";
+                }
+                if(session == 3){
+                    sessionChoisie = "Ete";
+                }
 
-            // Envoyer la commande handleLoadCourses au serveur avec le semestre demandé
-            objectOutputStream.writeObject(Server.LOAD_COMMAND + " " + sessionChoisie);
-            objectOutputStream.flush();
+                // Envoyer la commande handleLoadCourses au serveur avec le semestre demandé
+                objectOutputStream.writeObject(Server.LOAD_COMMAND + " " + sessionChoisie);
+                objectOutputStream.flush();
 
-            // Afficher la liste de cours
-            System.out.println("Les cours offerts pendant la session d' " + sessionChoisie + " sont:");
+                // Afficher la liste de cours
+                System.out.println("Les cours offerts pendant la session d' " + sessionChoisie + " sont:");
 
-            // Lire la liste de cours renvoyée par le serveur
-            ArrayList<Course> courses;
-            courses = (ArrayList<Course>) objectInputStream.readObject();
-            if (courses.isEmpty()) {
-                System.out.println("Aucun cours disponible pour la session sélectionnée.");
-            } else {
-                for (Course courseDisponible : courses) {
-                    System.out.println(courseDisponible);
+                // Lire la liste de cours renvoyée par le serveur
+                ArrayList<Course> courses;
+                courses = (ArrayList<Course>) objectInputStream.readObject();
+                if (courses.isEmpty()) {
+                    System.out.println("Aucun cours disponible pour la session sélectionnée.");
+                } else {
+                    for (Course courseDisponible : courses) {
+                        System.out.println(courseDisponible);
+                    }
+                }
+
+                // Demander à l'utilisateur s'il souhaite s'inscrire à un cours ou charger des cours pour un autre semestre
+
+                System.out.println("Pour continuer:");
+                System.out.println("1. Consulter les cours offerts pour une autre session");
+                System.out.println("2. Inscription à un cours");
+
+                int choix = scanner.nextInt();
+
+                if (choix == 1) {
+                    continueLoop = true;
+                } else if (choix == 2) {
+                    registerStudentToCourse();
+                    continueLoop = false;
+                } else {
+                    System.out.println("Choix invalide. Veuillez entrer 1, 2 ou 3.");
+                    continueLoop = true;
                 }
             }
-            String choix = scanner.nextLine();
-
-            // Demander à l'utilisateur s'il souhaite s'inscrire à un cours ou charger des cours pour un autre semestre
-
-            System.out.println("Pour continuer:");
-            System.out.println("1. Consulter les cours offerts pour une autre session");
-            System.out.println("2. Inscription à un cours");
-            
-            if (choix.equals("1")) {
-                System.out.println("aca te muestro");;
-            }
-            if (choix.equals("2")) {
-                registerStudentToCourse();
-            }
-
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
+
 
 
     // Méthode pour enregistrer un étudiant à un cours

5d61412d960d62f1e7380518f31ea3be93d27140
Hongil Kim <hon9il.kim@gmail.com>
2023-04-12 23:12:35 -0400

    commit #7 Hongil
    javadoc pour Server.java, EventHandler.java, ServerLauncher.java inclus, et creéation du dossier client_fx ouù on va faire TACHE3. Main.java, Controller.java, et sample.fxml sont creéeés dans ce dossier

--- IFT1025-TP2/src/main/java/server/ServerLauncher.java
+++ IFT1025-TP2/src/main/java/server/ServerLauncher.java
@@ -1,8 +1,17 @@
 package server;
 
+/**
+ * Cette classe lance un serveur qui écoute sur le port 1337.
+ */
 public class ServerLauncher {
+    /**
+     * Port sur lequel le serveur écoute.
+     */
     public final static int PORT = 1337;
-
+    /**
+     * Méthode principale qui lance le serveur et affiche un message de confirmation.
+     * @param args Les arguments de ligne de commande.
+     */
     public static void main(String[] args) {
         Server server;
         try {
